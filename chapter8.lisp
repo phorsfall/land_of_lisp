df0336e90bcbab7f4a46d76b2164a5e0a137bb32
Paul Horsfall <horsfallp@gmail.com>
2010-10-29 13:57:22 +0100

    Started chapter 8.

---
+++ chapter8.lisp
@@ -0,0 +1,25 @@
+(load "chapter7.lisp")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15)
+
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+			collect (edge-pair (random-node) (random-node)))))
+
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+		   (eql (car x) node))
+		 edge-list))
